51062b58bbe1697ba9f1ad9656b49897c22e4b26
davidshivaji <davidshivaji@gmail.com>
2022-05-06 19:03:33 +1000

    clear comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 import re
 
 # test
@@ -28,47 +28,28 @@ for module in modules:
         # might as well count lines
         for line in text.split('\n'):
             linecount += 1
-        # account for: from . import sessions
-        # y = re.search("(from\s)\.(.+)")
-        # matching the dot.
-        # if something has no group 1, it's not a local import.
             z = re.search("(from\s)(\.\s|\.[^\s]+)*(.*)(import)\s([^\s]+)", line)
-        # print(text)
-        # if there's a match in
+            # if the line has a match
             if z:
-                # print(z.groups())
+                # if something has no group 1, it's not a local import.
                 if z.groups()[1]:
                     if z.groups()[1] == ". ":
                         mod = z.groups()[4]
                     else:
-                        # mod = ".auth" (in the case of models.py)
                         mod = z.groups()[1][1:]
-                        # mod = 0
 
-                # print(mod)
-                # should fucking well have models and auth
                     dependencies.append(mod)
-        # print(module, dependencies)
-        # tree[module].append(mod)
-            # print('niet')
+
         tree[module] = list(set(dependencies))
-                # it should only be going if it's in there.
-
-        # tree[module].append(8)
-            # if line.startswith('from') or line.startswith('import'):
-                # print(line)
 
 
-
-# print(tree)
-print('\n')
-for item, value in tree.items():
-    print(item, value)
+print(json.dumps(tree))
+# for item, value in tree.items():
+#     print(item, value)
 
 print(linecount)
 
+# now with that tree:
 
-        # for line in file,
-    # if there's a dot,
-        # module = after that dot until a space
-        # if module in files
+# what is the data structure?
+# it's a fuckin tree.
